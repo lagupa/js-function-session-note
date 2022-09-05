e9130c5ea8acaf6a13691c083cb0e1243ebcacba
Lagu Longa <lagustephen@gmail.com>
2022-09-05 17:03:20 -0400

    clear form fields

--- contact-app-rj/src/components/CreatContact.js
+++ contact-app-rj/src/components/CreatContact.js
@@ -1,49 +1,68 @@
-import React from 'react'
+import React from 'react';
 
 const CreateContact = ({ appX, isBlack, saveContact }) => {
+  const initalFormState = {
+    firstName: '',
+    lastName: '',
+    email: '',
+  };
+  const [contact, setContact] = React.useState(initalFormState);
 
-    const [contact, setContact] = React.useState({
-        firstName:'',
-        lastName:"",
-        email:""
-    })
+  const handleChange = (e) => {
+    setContact({
+      ...contact,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleChange = (e) => {
-        setContact({
-            ...contact,
-            [e.target.name]:e.target.value
-        })
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // sending to App.js
+    saveContact(contact);
 
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-        // save to database here
-        // sending to App.js
-        saveContact(contact)
-    }
+    // save to database here
 
+    // clear form fields
+    setContact(initalFormState);
+  };
 
-
-    const {firstName, lastName, email} = contact;
+  const { firstName, lastName, email } = contact;
 
   return (
     <div>
-        {appX}
-        <h2>Create Contact</h2>
-        <div style = {{
-            background:isBlack ? "black": null,
-            color: isBlack ? "white": null,
-            padding:"1rem"
-        }}>
-       <form onSubmit={handleSubmit}>
-            <input type="text"  value={firstName} name="firstName" onChange={handleChange} />
-            <input type="text" value={lastName} name="lastName" onChange={handleChange} />
-            <input type="email" value={email} name="email" onChange={handleChange} />
-            <button type='submit'>create</button>
+      {appX}
+      <h2>Create Contact</h2>
+      <div
+        style={{
+          background: isBlack ? 'black' : null,
+          color: isBlack ? 'white' : null,
+          padding: '1rem',
+        }}
+      >
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={firstName}
+            name="firstName"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            value={lastName}
+            name="lastName"
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            value={email}
+            name="email"
+            onChange={handleChange}
+          />
+          <button type="submit">create</button>
         </form>
-       </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreateContact
+export default CreateContact;
